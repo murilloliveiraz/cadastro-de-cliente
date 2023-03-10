1baecd4b763ff49f4eccc8eb7997f59dbaa5376c
murillo <113298979+murilloliveiraz@users.noreply.github.com>
2023-03-10 10:17:43 -0300

    exercicios do 1bimestre do SENAI

---
+++ exjava/ifelse.java
@@ -0,0 +1,9 @@
+public class ifelse {
+    public static void main (String [] args){
+        double itemCost = 30.99;
+
+        if (itemCost > 24.00){
+            System.out.println("high vale item!");
+        }
+    }
+}
